3c87bb965875a87e1dc5affa77f4371ce7b64b4e
Damien Heulin <damienheulin87@gmail.com>
2022-05-10 17:07:24 +0200

    Guest typo

--- pages/guest.vue
+++ pages/guest.vue
@@ -8,7 +8,7 @@ const user = useState('user', () => null)
 
 <template>
     <div>
-        <h1 class="mb-3 text-lg font-bold text-light-100">Page privée</h1>
+        <h1 class="mb-3 text-lg font-bold text-light-100">Page invitée</h1>
         <p class="mb-3 text-light-100">Cette page ne devrait s'afficher que si l'utilisateur n'est <span
                 class="font-bold">PAS</span> connecté</p>
         <code class="mb-3 block text-light-100">            
