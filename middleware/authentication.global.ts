692a29b447cab83939b688a6fd91b647b026ea1c
Damien Heulin <damienheulin87@gmail.com>
2022-05-10 16:02:37 +0200

    Removed delay function

--- middleware/authentication.global.ts
+++ middleware/authentication.global.ts
@@ -1,17 +1,11 @@
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
 export default defineNuxtRouteMiddleware(async (to) => {
     if (process.server) { // Only call on initial render ?
         const user = useState('user', () => null);
 
         try {
-            await delay(500);
-
             user.value = await $fetch('/api/auth/me', {
                 headers: useRequestHeaders(['cookie'])
             });
-
-            await delay(500);
         } catch (error) {
             console.log(error);
         }
