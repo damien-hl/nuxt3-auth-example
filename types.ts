4b275bcdff2254073908b4c3a4a2976c4bb2a258
Damien Heulin <damienheulin87@gmail.com>
2022-07-01 09:55:31 +0200

    chore: tsconfig

--- types.ts
+++ types.ts
@@ -1,8 +1,8 @@
-export type User = {
-    id: string;
-    email: string;
-    password: string;
-    roles: string[];
+export interface User {
+  id: string
+  email: string
+  password: string
+  roles: string[]
 }
 
-export type UserWithoutPassword = Omit<User, "password">
+export type UserWithoutPassword = Omit<User, 'password'>
