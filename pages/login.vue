fe52a1030efa62562c0e3ead5b41b1800d6fc213
Damien Heulin <damienheulin87@gmail.com>
2022-07-12 17:33:51 +0200

    refactor: login form

--- pages/login.vue
+++ pages/login.vue
@@ -5,38 +5,11 @@ definePageMeta({
 
 const currentUser = useAuthUser()
 const isAdmin = useAdmin()
-const { login } = useAuth()
 
-const form = reactive({
-  data: {
-    email: 'admin@gmail.com',
-    password: 'password',
-    rememberMe: false,
-  },
-  error: '',
-  pending: false,
-})
+async function onLoginSuccess() {
+  const redirect = isAdmin.value ? '/admin' : '/private'
 
-async function onLoginClick() {
-  try {
-    form.error = ''
-    form.pending = true
-
-    await login(form.data.email, form.data.password, form.data.rememberMe)
-
-    const redirect = isAdmin.value ? '/admin' : '/private'
-
-    await navigateTo(redirect)
-  }
-  catch (error: any) {
-    console.error(error)
-
-    if (error.data.message)
-      form.error = error.data.message
-  }
-  finally {
-    form.pending = false
-  }
+  await navigateTo(redirect)
 }
 </script>
 
@@ -46,28 +19,7 @@ async function onLoginClick() {
 
     <PageDescription description="This page should only be accessible to guests." />
 
-    <p v-if="form.error" mb-3 text-red-500>
-      {{ form.error }}
-    </p>
-
-    <form mb-3 flex flex-wrap gap-3 @submit.prevent="onLoginClick">
-      <div w-full>
-        <input id="remember-me" v-model="form.data.rememberMe" type="checkbox">
-
-        <label for="remember-me" ml-1 text-light-100>Remember me</label>
-      </div>
-
-      <input v-model="form.data.email" type="email" placeholder="Email" required>
-
-      <input v-model="form.data.password" type="password" placeholder="Password" required>
-
-      <button
-        type="submit" :disabled="form.pending"
-        py-1 px-2 rounded bg-light-100 hover="bg-light-700" transition-colors
-      >
-        Login
-      </button>
-    </form>
+    <FormLogin @success="onLoginSuccess" />
 
     <div mb-3 flex gap-3>
       <div>
