81d032c2d5ec9e0a059f8a36420318f71c00d553
Damien Heulin <damienheulin87@gmail.com>
2022-08-11 17:05:25 +0200

    fix: typo on cookieName in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   runtimeConfig: {
-    cookieNAME: process.env.COOKIE_NAME || '__session',
+    cookieName: process.env.COOKIE_NAME || '__session',
     cookieSecret: process.env.COOKIE_SECRET || 'secret',
   },
   modules: [
