2a107cae9020995e116fafad0802c0f70131986f
Damien Heulin <damienheulin87@gmail.com>
2022-08-12 09:29:33 +0200

    fix: cookie name in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   runtimeConfig: {
-    cookieName: process.env.COOKIE_NAME || '__session',
+    cookieName: process.env.COOKIE_NAME || '__token',
     cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || '1000 * 60 * 60 * 24', 10), // 1 day
     cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || '1000 * 60 * 60 * 24 * 7', 10), // 7 days
     jwtSecret: process.env.JWT_SECRET || 'superdupersecret',
