c3880e3eada84394005a6a4a677952f24f021e37
Damien Heulin <damienheulin87@gmail.com>
2022-09-03 20:06:43 +0200

    chore: upgrade to nuxt rc9

--- e2e/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('go to /', async ({ page }) => {
-  await page.goto('/')
-
-  expect(await page.title()).toBe('Simple authentication example using Nuxt3')
-})
