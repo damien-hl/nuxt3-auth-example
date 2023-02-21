e53cc45503566d9b907a7e6097f00a5153db54e5
Damien Heulin <damienheulin87@gmail.com>
2023-02-21 13:22:03 +0100

    chore: setup warning when no auth password env is provided

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
+if (!process.env.NUXT_AUTH_PASSWORD) {
+  // eslint-disable-next-line no-console
+  console.warn('NUXT_AUTH_PASSWORD is not set. Using default password.')
+  process.env.NUXT_AUTH_PASSWORD = 'supersecretsupersecretsupersecret'
+}
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/color-mode', '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
