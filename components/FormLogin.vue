0b0817bc9e0b8a23ee9e8e345ec70808b72cbb49
Damien Heulin <damienheulin87@gmail.com>
2023-12-30 15:29:03 +0100

    feat: better styles for form login inputs and button

--- components/FormLogin.vue
+++ components/FormLogin.vue
@@ -32,29 +32,34 @@ async function onLoginClick() {
 </script>
 
 <template>
-  <p v-if="form.error" class="mb-3 text-red-500">
+  <p v-if="form.error" class="mb-3 text-sm text-red-500">
     {{ form.error }}
   </p>
   <form class="mb-3 flex flex-wrap gap-3" @submit.prevent="onLoginClick">
-    <div class="w-full">
-      <input id="remember-me" v-model="form.data.rememberMe" type="checkbox" />
-
-      <label for="remember-me" class="ml-1 text-light-100">Remember me</label>
+    <div class="w-full flex items-center">
+      <input id="remember-me" v-model="form.data.rememberMe" type="checkbox" class="w-4 h-4 accent-slate-700" />
+      <label for="remember-me" class="ml-1 text-sm text-light-100">Remember me</label>
     </div>
-    <input v-model="form.data.email" type="email" placeholder="Email" class="bg-white text-slate-950" required />
+    <input
+      v-model="form.data.email"
+      type="email"
+      placeholder="Email"
+      class="px-3 py-1.5 border rounded border-slate-700 bg-slate-900 text-sm focus:outline-none focus:ring-2 focus:ring-slate-700 focus:border-transparent"
+      required
+    />
     <input
       v-model="form.data.password"
       type="password"
       placeholder="Password"
-      class="bg-white text-slate-950"
+      class="px-3 py-1.5 border rounded border-slate-700 bg-slate-900 text-sm focus:outline-none focus:ring-2 focus:ring-slate-700 focus:border-transparent"
       required
     />
     <button
       type="submit"
       :disabled="form.pending"
-      class="py-1 px-2 rounded bg-light-100 text-slate-950 hover:bg-light-700 transition-colors"
+      class="px-3 py-1.5 rounded bg-light-100 font-semibold text-sm text-slate-950 hover:bg-light-700 focus:outline-none focus:bg-light-700 transition-colors"
     >
-      Login
+      Sign in
     </button>
   </form>
 </template>
