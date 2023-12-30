c17b8410a6a52fa38482e654e29e928dfaaeca7e
Damien Heulin <damienheulin87@gmail.com>
2023-12-30 15:48:08 +0100

    feat: better looking for login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,93 @@
 <script lang="ts" setup>
 definePageMeta({
   middleware: ["guest-only"],
+  layout: false,
 });
 
-const currentUser = useAuthUser();
+const { login } = useAuth();
+
+const form = reactive({
+  data: {
+    email: "admin@gmail.com",
+    password: "password",
+    rememberMe: false,
+  },
+  error: "",
+  pending: false,
+});
+
 const isAdmin = useAdmin();
 
-async function onLoginSuccess() {
-  const redirect = isAdmin.value ? "/admin" : "/private";
+async function onLoginClick() {
+  try {
+    form.error = "";
+    form.pending = true;
 
-  await navigateTo(redirect);
+    await login(form.data.email, form.data.password, form.data.rememberMe);
+
+    const redirect = isAdmin.value ? "/admin" : "/private";
+    await navigateTo(redirect);
+  } catch (error: any) {
+    console.error(error);
+
+    if (error.data.message) form.error = error.data.message;
+  } finally {
+    form.pending = false;
+  }
 }
 </script>
 
 <template>
-  <div>
-    <PageTitle title="Login page" />
-    <PageDescription description="This page should only be accessible to guests." />
-    <FormLogin @success="onLoginSuccess" />
-    <div class="mb-3 flex gap-3">
-      <div>
-        <p class="text-light-100">Try this user credentials:</p>
-        <p class="text-light-100">
-          email: <code>user@gmail.com</code>
-          <br />
-          password: <code>password</code>
+  <div class="min-h-screen flex flex-col">
+    <header>
+      <h1 class="my-24 font-bold text-2xl text-center">Login to your account</h1>
+    </header>
+    <main class="mx-auto p-8 max-w-xs w-full flex-1">
+      <form @submit.prevent="onLoginClick">
+        <p
+          v-if="form.error"
+          class="mb-3 px-3 py-1.5 w-full border rounded border-red-400 text-sm text-center text-red-400"
+        >
+          {{ form.error }}
         </p>
+        <div class="mb-3">
+          <input
+            v-model="form.data.email"
+            type="email"
+            placeholder="Email"
+            class="px-3 py-1.5 w-full border rounded border-slate-700 bg-slate-900 text-sm focus:outline-none focus:ring-2 focus:ring-slate-700 focus:border-transparent"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            v-model="form.data.password"
+            type="password"
+            placeholder="Password"
+            class="px-3 py-1.5 w-full border rounded border-slate-700 bg-slate-900 text-sm focus:outline-none focus:ring-2 focus:ring-slate-700 focus:border-transparent"
+            required
+          />
+        </div>
+        <div class="mb-3 flex justify-end items-center">
+          <label for="remember-me" class="mr-1 text-sm text-light-100">Remember me</label>
+          <input id="remember-me" v-model="form.data.rememberMe" type="checkbox" class="w-4 h-4 accent-slate-700" />
+        </div>
+        <div class="mb-6">
+          <button
+            type="submit"
+            :disabled="form.pending"
+            class="px-3 py-1.5 w-full rounded bg-light-100 font-semibold text-sm text-slate-950 hover:bg-light-700 focus:outline-none focus:bg-light-700 transition-colors"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+      <div class="text-center">
+        <NuxtLink to="/" class="text-xs">Go home</NuxtLink>
       </div>
-      <div>
-        <p class="text-light-100">Try this admin credentials:</p>
-        <p class="text-light-100">
-          email: <code>admin@gmail.com</code>
-          <br />
-          password: <code>password</code>
-        </p>
-      </div>
-    </div>
-    <PageUser :user="currentUser" />
+    </main>
+    <footer>
+      <div class="my-12 flex justify-center"></div>
+    </footer>
   </div>
 </template>
